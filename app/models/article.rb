5fc811829e837dc70c7151adbbbef4bfd064367e
arunkumarkassa <karun.kumar@actualpixel.in>
2024-04-05 16:49:34 +0530

    new changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_one_attached :image
   validates :title, presence: true, length: { minimum: 4, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
